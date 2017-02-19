5672e98e755b959f1921010f528080ffc0da27a8
Jonas Sjöberg <jomeganas@gmail.com>
2017-02-19 23:41:48 +0100

    Censor comments.

--- src/main/java/lnu/resources/RemoveBookResource.java
+++ src/main/java/lnu/resources/RemoveBookResource.java
@@ -21,12 +21,8 @@ import javax.ws.rs.core.Response;
 @Path("/books")
 public class RemoveBookResource
 {
-    // Just what the hell is going on? This is all such a huge mess of stuff.
-
     // TODO: Read the 30000 lines of minified server code? (!)
-    // TODO: Figure out how to re-compile without rebooting the fucking VM? (!)
-
-    // God damn I hate modern web development.
+    // TODO: Figure out how to re-compile without rebooting the F*****G VM? (!)
 
     @DELETE
     @Path("{book_id}")
