943f249dc4e05b79936fb9224f02585213823998
Jonas Sjöberg <jomeganas@gmail.com>
2017-03-05 19:28:52 +0100

    Remove unused import.

--- src/main/java/lnu/dao/booksDAO.java
+++ src/main/java/lnu/dao/booksDAO.java
@@ -24,7 +24,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
