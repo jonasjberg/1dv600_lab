f2743cdc63ae765e27b9563e8287cc9302a350c5
Jonas Sjöberg <jomeganas@gmail.com>
2017-03-05 19:26:17 +0100

    Modify the 'boodsDAO'.

--- src/main/java/lnu/dao/booksDAO.java
+++ src/main/java/lnu/dao/booksDAO.java
@@ -14,20 +14,36 @@ package lnu.dao;
 
 import lnu.models.Book;
 import lnu.models.Catalog;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class booksDAO
 {
-    private static final String FILE_NAME = "books.xml";
-    private final Catalog bookCatalog = new Catalog();;
+    private static final String  FILE_NAME   = "books.xml";
+    private static final Catalog bookCatalog = new Catalog();
 
     public booksDAO()
     {
+    }
+
+    public void getBooks()
+    {
+        File localBookXmlFile = getFileFromPath(FILE_NAME);
+        // bookCatalog.addBooks(readBooksFromFile(localBookXmlFile));
+        bookCatalog.addBook(jaxbXMLToObject(localBookXmlFile));
     }
 
     /**
@@ -35,11 +51,12 @@ public class booksDAO
      *
      * @return The
      */
-    private static Book jaxbXMLToObject() {
+    private static Book jaxbXMLToObject(File file)
+    {
         try {
             JAXBContext  context = JAXBContext.newInstance(Book.class);
             Unmarshaller un      = context.createUnmarshaller();
-            Book         book    = (Book) un.unmarshal(new File(FILE_NAME));
+            Book         book    = (Book) un.unmarshal(file);
             return book;
         } catch (JAXBException e) {
             /* TODO: Error handling, logging. */
@@ -49,4 +66,47 @@ public class booksDAO
         return null;
     }
 
+    private File getFileFromPath(String path)
+    {
+        File file = null;
+
+        try {
+            file = new File(path);
+        } catch (Exception e) {
+            System.out.printf("[ERROR]: %s\n", e.toString());
+        }
+
+        return file;
+    }
+
+    /**
+     * Reads books from an XML file.
+     *
+     * Source: Big Java Late Object - Horstmann C. 2013 Wiley
+     *         Chapter "23.2 Parsing XML Documents" from the "web-chapters"
+     *         available at the Book website.
+     *
+     * @param file The XML file to read.
+     * @return
+     */
+    public ArrayList<Book> readBooksFromFile(File file)
+    {
+        ArrayList<Book> list = new ArrayList<>();
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder  = factory.newDocumentBuilder();
+            Document        document = builder.parse(file);
+
+            NodeList nodeList = document.getElementsByTagName("book");
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 }
